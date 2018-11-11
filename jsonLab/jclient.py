7f92f9246c33b582403ae9da1c0694ab353dc44a
student <Sebsatianq1290@gmail.com>
2018-11-11 13:59:06 -0700

    added folder for tree/json lab

---
+++ jsonLab/jclient.py
@@ -0,0 +1,52 @@
+# minimalistic client example from 
+# https://github.com/seprich/py-bson-rpc/blob/master/README.md#quickstart
+
+import socket
+from bsonrpc import JSONRpc
+from bsonrpc.exceptions import FramingError
+from bsonrpc.framing import (
+	JSONFramingNetstring, JSONFramingNone, JSONFramingRFC7464)
+from node import * #import all methods in node
+
+# creates socket connection
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('localhost', 50001))
+
+#connects to server
+rpc = JSONRpc(s,framing_cls=JSONFramingNone)
+server = rpc.get_peer_proxy()
+
+#execute in server?
+#result = server.swapper('Hello World!')
+#test = server.nop('Friend!')
+# "!dlroW olleH"
+#print(result)
+#print(test)
+
+print(server.nop({1:[2,3]}))
+
+#create a method in server that takes in a tree graph
+#send in as server.(encodedInfo)
+#on server side, there should be a decoded that's available to take
+#change it to a data type there then incremenet
+#but then return the same data would have to be encoded and then decoded as well
+
+#TODOLIST FOR NOW
+#1.)Send as encoded json data
+#2.)Deconstruct on other side to create tree
+#3.)Increment Print
+#graph code------------------------------------------------------------------------------
+leaf1 = node("leaf1")
+leaf2 = node("leaf2")
+root  = node("root", [leaf1,leaf2])
+
+#print("tree b4 ++")
+root.show();
+
+#supposed increment remotely
+server.incrementTree(JSONEncoder().encode(root))
+
+#print.("graph after increment")
+root.show()
+
+rpc.close() # Closes the socket 's' also
